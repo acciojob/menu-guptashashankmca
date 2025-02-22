1001b92e0dca0636e8353dd244d49dd5efaf5a3f
guptashashankmca <shashankgupta.mca@outlook.com>
2025-02-22 16:58:02 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,119 +1,156 @@
 import React, { useState } from 'react'
-import cards from './cards'
+import CompA from './CompA';
+
 
 const data = [
     {
-        id: 1,
-        title:'buttermilk pancakes',
-        category:'breakfast',
-        price:15.99,
-        img:'./images/item-1.jpeg',
-        desc:`I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`
+      id: 1,
+      title: 'buttermilk pancakes',
+      category: 'breakfast',
+      price: 15.99,
+      img: './images/item-1.jpeg',
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
-        id: 2,
-        title:'diner double',
-        category:'lunch',
-        price:13.99,
-        img:'./images/item-2.jpeg',
-        desc:`vaporware iPhone mumblecore selvage raw denim slow-carb salvia, banjo tumeric`
+      id: 2,
+      title: 'diner double',
+      category: 'lunch',
+      price: 13.99,
+      img: './images/item-2.jpeg',
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
-        id: 3,
-        title:'godzilla milkshake',
-        category:'shakes',
-        price:6.99,
-        img:'./images/item-3.jpeg',
-        desc:`ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral`
+      id: 3,
+      title: 'godzilla milkshake',
+      category: 'shakes',
+      price: 6.99,
+      img: './images/item-3.jpeg',
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
-        id: 4,
-        title:'country delight',
-        category:'breakfast',
-        price:20.99,
-        img:'./images/item-4.jpeg',
-        desc:`Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut`
+      id: 4,
+      title: 'country delight',
+      category: 'breakfast',
+      price: 20.99,
+      img: './images/item-4.jpeg',
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
-        id: 5,
-        title:'egg attack',
-        category:'lunch',
-        price:22.99,
-        img:'./images/item-5.jpeg',
-        desc:`franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up`
+      id: 5,
+      title: 'egg attack',
+      category: 'lunch',
+      price: 22.99,
+      img: './images/item-5.jpeg',
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
-        id: 6,
-        title:'oreo dream',
-        category:'shakes',
-        price:18.99,
-        img:'./images/item-6.jpeg',
-        desc:`Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`
-     },
-    {
-        id: 7,
-        title:'bacon overflow',
-        category:'breakfast',
-        price:8.99,
-        img:'./images/item-7.jpeg',
-        desc:`carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird on it`
+      id: 6,
+      title: 'oreo dream',
+      category: 'shakes',
+      price: 18.99,
+      img: './images/item-6.jpeg',
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
-        id: 8,
-        title:'american classic',
-        category:'lunch',
-        price:12.99,
-        img:'./images/item-8.jpeg',
-        desc:`on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut`
+      id: 7,
+      title: 'bacon overflow',
+      category: 'breakfast',
+      price: 8.99,
+      img: './images/item-7.jpeg',
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
-        id: 9,
-        title:'quarantine buddy',
-        category:'shakes',
-        price:16.99,
-        img:'./images/item-9.jpeg',
-        desc:`skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing`
+      id: 8,
+      title: 'american classic',
+      category: 'lunch',
+      price: 12.99,
+      img: './images/item-8.jpeg',
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
-]
+    {
+      id: 9,
+      title: 'quarantine buddy',
+      category: 'shakes',
+      price: 16.99,
+      img: './images/item-9.jpeg',
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
+  
+
 
 const App = () => {
-   const [menuData, setMenuData] = useState(data)
-   const [filterMenuData, setFilterMenuData] = useState(data)
- 
+
+    const[items, setItems] = useState(data)
+    const[filteredItems, setFilteredItems] = useState(data)
+
+
+    
+
   return (
-    <div style={{width:"80vw", margin:"auto"}}>
-            <h1 style={{margin:"auto", border:"1px solid black", width:"100px"}}>Our Menu</h1>
-            <div>
-            <button onClick={()=>{
-                setFilterMenuData(menuData)
+    <div id='main'>
+        
+        <h1>Our Menu</h1>
+
+        <div>
+            <button onClick={() => {
+                const cat = "all"
+
+                // const nArr = items.filter((item) => {
+                //     return item.category == cat
+                // })
+
+                setFilteredItems(data)
+                // console.log(nArr)
+
+
             }}>All</button>
+            <button id='filter-btn-1' onClick={() => {
+                const cat = "breakfast"
 
-            <button onClick={() =>{
-                const nArr = menuData.filter((item)=>{return item.category == "breakfast"})
+                const nArr = items.filter((item) => {
+                    return item.category == cat
+                })
 
-                setFilterMenuData(nArr)
+                setFilteredItems(nArr)
+                // console.log(nArr)
+
 
             }}>Breakfast</button>
+            <button id='filter-btn-2' onClick={() => {
+                const cat = "lunch"
 
-            <button onClick={() =>{
-                const nArr = menuData.filter((item)=>{return item.category == "Lunch"})
+                const nArr = items.filter((item) => {
+                    return item.category == cat
+                })
 
-                setFilterMenuData(nArr)}}>Lunch</button>
+                setFilteredItems(nArr)
+                // console.log(nArr)
 
-            <button onClick={() =>{
-                const nArr = menuData.filter((item)=>{return item.category == "Shakes"})
 
-                setFilterMenuData(nArr)}}>Shakes</button>
+            }}>Lunch</button>
+            <button id='filter-btn-3' onClick={() => {
+                const cat = "shakes"
 
-            </div>
-        <div style={{display:"grid", gridTemplateColumns:"1fr 1fr", margin}} >
-            {menuData.map(()=>{
-                return <cards key={itme.id} item={item.title} price={item.price} category={item.category}/>
+                const nArr = items.filter((item) => {
+                    return item.category == cat
+                })
+
+                setFilteredItems(nArr)
+                // console.log(nArr)
+
+
+            }}>Shakes</button>
+        </div>
+
+        <div style={{display:'flex', flexWrap : "wrap"}}>
+            {filteredItems.map((item) => {
+                return <CompA item={item} />
             })}
         </div>
+
     </div>
   )
 }
-
 
 export default App
